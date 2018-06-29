7a33bc8bc5306ff187f0364512f34bc26f1d0ce3
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2018-06-29 05:05:17 +0000

    - добавил функцию удаления из выбранных
    git-svn-id: svn://nanorit.com/jsapi/src@11211 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

--- src/app/main/documentation/components-third-party/components-third-party.module.ts
+++ src/app/main/documentation/components-third-party/components-third-party.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatGridListModule,
+         MatDividerModule, MatFormFieldModule, MatMenuModule, MatSelectModule, MatTableModule, MatTabsModule } from '@angular/material';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseSidebarModule } from '@fuse/components';
+import { FuseWidgetModule } from '@fuse/components/widget/widget.module';
 
 import { GoogleMapsModule } from 'app/main/documentation/components-third-party/google-maps/google-maps.module';
 import { DocsComponentsThirdPartyNgxDatatableComponent } from 'app/main/documentation/components-third-party/datatable/ngx-datatable.component';
@@ -25,10 +28,20 @@ const routes = [
         MatButtonModule,
         MatCheckboxModule,
         MatIconModule,
+        MatGridListModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatMenuModule,
+        MatSelectModule,
+        MatTableModule,
+        MatTabsModule,
+        
 
         NgxDatatableModule,
 
         FuseSharedModule,
+        FuseSidebarModule,
+        FuseWidgetModule,
 
         GoogleMapsModule
     ]
--- src/app/main/documentation/components-third-party/datatable/ngx-datatable.component.ts
+++ src/app/main/documentation/components-third-party/datatable/ngx-datatable.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit, isDevMode} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
+import { fuseAnimations } from '@fuse/animations';
 
 //import {Model} from '/model';
 //import {MessageService} from '/message.service';
@@ -12,7 +13,8 @@ import {currentUser} from '../../../../_models/currentuser';
 @Component({
     selector: 'docs-components-third-party-ngx-datatable',
     templateUrl: './ngx-datatable.component.html',
-    styleUrls: ['./ngx-datatable.component.scss']
+    styleUrls: ['./ngx-datatable.component.scss'],
+    animations   : fuseAnimations
 })
 export class DocsComponentsThirdPartyNgxDatatableComponent implements OnInit, OnDestroy {
     rows: any[];
@@ -61,21 +63,36 @@ export class DocsComponentsThirdPartyNgxDatatableComponent implements OnInit, On
         this._httpClient.get(`${this.api_url}/apps/api/rest.php?action=model&do=get_data&session_key=${this.currentUser.session_key}`)
             .pipe(takeUntil(this._unsubscribeAll))
             .subscribe((contacts: any) => {
-                console.log(contacts.rows);
+                //console.log(contacts.rows);
                 this.rows = contacts.rows;
                 this.loadingIndicator = false;
             });
+        
+        const load_selected_request = {action:'model', do:'load_selected', session_key:this.currentUser.session_key};
+        
+        this._httpClient.post(`${this.api_url}/apps/api/rest.php`, load_selected_request)
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((result: any) => {
+                console.log('selected > ');
+                console.log(result);
+                if ( result ) {
+                    this.selected = result.selected;
+                }
+                
+                this.loadingIndicator = false;
+            });
+            
     }
     
     onSelect({selected}) {
-        console.log('Select Event', selected, this.selected);
-        console.log(selected.length);
+        //console.log('Select Event', selected, this.selected);
+        //console.log(selected.length);
         const body = {action:'model', do:'select', session_key:this.currentUser.session_key, selected_items: selected};
         
         //?action=model&do=select&session_key=${this.currentUser.session_key}
-        console.log(body);
+        //console.log(body);
         
-        this._httpClient.post(`${this.api_url}/apps/api/rest.php?action1=11`, body)
+        this._httpClient.post(`${this.api_url}/apps/api/rest.php`, body)
             .subscribe((contacts: any) => {
                 console.log(contacts);
             });
@@ -86,6 +103,29 @@ export class DocsComponentsThirdPartyNgxDatatableComponent implements OnInit, On
         console.log(this.selected.length);
         
     }
+    
+    delete_selection(item_id: string) {
+        console.log('Delete selection', item_id);
+        const body = {action:'model', do:'delete_selection', session_key:this.currentUser.session_key, item_id: item_id};
+        
+        this._httpClient.post(`${this.api_url}/apps/api/rest.php`, body)
+            .subscribe((result: any) => {
+                console.log('selected > ');
+                console.log(result);
+                if ( result ) {
+                    this.selected = result.selected;
+                } else {
+                    //this.selected = [];
+                }
+                
+                //this.selected.splice(0, this.selected.length);
+                //this.selected.push(...result.selected);
+                
+                this.loadingIndicator = false;
+            });
+
+    }
+    
 
     onActivate(event) {
         //console.log('Activate Event', event);
