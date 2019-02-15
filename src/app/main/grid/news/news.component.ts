55425ffc49e7548e3635e976bb9ea86314753dfd
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-02-15 02:34:01 +0000

    - news.component.ts
    git-svn-id: svn://nanorit.com/jsapi/src@11867 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/main/grid/news/news.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { GridComponent } from 'app/main/grid/grid.component';
+import { fuseAnimations } from '@fuse/animations';
+import { takeUntil } from 'rxjs/operators';
+
+
+@Component({
+    selector: 'data-grid',
+    templateUrl: '../grid.component.html',
+    styleUrls: ['../grid.component.scss'],
+    animations: fuseAnimations
+})
+export class NewsComponent extends GridComponent {
+    get_grid_items(params: any) {
+        let grid_item;
+        grid_item = ['news_id', 'title'];
+        return grid_item;
+    }
+
+    setup_apps() {
+        this.app_name = 'news';
+        this.primary_key = 'news_id';
+
+        this.data_columns = [
+            {
+                headerTemplate: this.hdrTpl,
+                type: 'primary_key',
+                ngx_name: 'news_id.title',
+                model_name: 'news_id',
+                title: 'ID',
+                prop: 'news_id.value'
+            },
+            {
+                headerTemplate: this.hdrTpl,
+                type: 'safe_string',
+                ngx_name: 'news.title',
+                model_name: 'title',
+                title: 'Заголовок',
+                prop: 'title.value'
+            },
+            {
+                headerTemplate: this.hdrTpl,
+                cellTemplate: this.controlTmpl,
+                type: 'primary_key',
+                ngx_name: 'news_id.title',
+                model_name: 'news_id',
+                title: '',
+                prop: 'news_id.value'
+            },
+
+
+        ];
+    }
+}
