42c5238a5aeb88c957fd9edb39ebc520987c6e1d
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-02-21 05:41:49 +0000

    - gallery
    git-svn-id: svn://nanorit.com/jsapi/src@11898 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/main/gallery/gallery.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+
+@Component({
+    selector: 'gallery-component',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.scss'],
+})
+export class GalleryComponent implements OnInit {
+    galleryOptions: NgxGalleryOptions[];
+
+    @Input("galleryImages")
+    galleryImages: NgxGalleryImage[];
+
+
+    ngOnInit(): void {
+
+        this.galleryOptions = [
+            {
+                width: '600px',
+                height: '400px',
+                thumbnailsColumns: 4,
+                imageAnimation: NgxGalleryAnimation.Slide,
+                thumbnailActions: [{ icon: 'fa fa-times-circle', onClick: this.deleteImage.bind(this), titleText: 'delete' }]
+            },
+            // max-width 800
+            {
+                breakpoint: 800,
+                width: '100%',
+                height: '600px',
+                imagePercent: 80,
+                thumbnailsPercent: 20,
+                thumbnailsMargin: 20,
+                thumbnailMargin: 20
+            },
+            // max-width 400
+            {
+                breakpoint: 400,
+                preview: false
+            }
+        ];
+
+    }
+
+    deleteImage() {
+        console.log('delete image');
+    }
+}
