d0b882ef450258e438eb5e0ca1fc61ed7b8752ee
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-02-26 09:56:27 +0000

    - динамический размер галереи
    git-svn-id: svn://nanorit.com/jsapi/src@11949 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

--- src/app/main/gallery/gallery.component.ts
+++ src/app/main/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ViewChild,TemplateRef, ElementRef, SimpleChange, OnChanges, IterableDiffers, DefaultIterableDiffer } from '@angular/core';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryModule, NgxGalleryComponent } from 'ngx-gallery';
 
 @Component({
     selector: 'gallery-component',
@@ -11,7 +11,7 @@ export class GalleryComponent implements OnInit {
     private differ: DefaultIterableDiffer<any>;
 
     //gallery_object: any;
-    @ViewChild('gallery_object') gallery_object: ElementRef;
+    @ViewChild('gallery_object') gallery_object: ElementRef<NgxGalleryComponent>;
 
     @Input("galleryImages")
     galleryImages: NgxGalleryImage[];
@@ -23,18 +23,37 @@ export class GalleryComponent implements OnInit {
     ngOnInit(): void {
         this.differ =
             <DefaultIterableDiffer<any>>this.differs.find(this.galleryImages).create();
+        let rows_number_calc = Math.ceil(this.galleryImages.length / 4);
+        if (rows_number_calc < 1) {
+            rows_number_calc = 1;
+        }
+        let height_calc = rows_number_calc * 100;
 
 
         this.galleryOptions = [
             {
                 width: '100%',
-                height: '400px',
-                thumbnailsColumns: 7,
+                height: height_calc + 'px',
+                image: false,
+                "arrowPrevIcon": "fa fa-arrow-circle-o-left",
+                "arrowNextIcon": "fa fa-arrow-circle-o-right",
+                "closeIcon": "fa fa-window-close",
+                "fullscreenIcon": "fa fa-arrows",
+                "spinnerIcon": "fa fa-refresh fa-spin fa-3x fa-fw",
+                "previewFullscreen": true,
+                "thumbnailsOrder": 2,
+                thumbnailsColumns: 4,
+                thumbnailsRows: rows_number_calc,
                 previewCloseOnClick: true,
                 imageBullets: true,
                 imageInfinityMove: true,
                 imageAnimation: NgxGalleryAnimation.Slide,
-                thumbnailActions: [{ icon: 'fa fa-times-circle', onClick: this.deleteImage.bind(this), titleText: 'delete' }]
+                thumbnailActions: [
+                    { icon: 'fa fa-chevron-left fa-sm', onClick: this.deleteImage.bind(this), titleText: 'выше' },
+                    { icon: 'fa fa-times-circle fa-sm', onClick: this.deleteImage.bind(this), titleText: 'удалить' },
+                    { icon: 'fa fa-chevron-right fa-sm', onClick: this.deleteImage.bind(this), titleText: 'ниже' },
+                    { icon: 'fa fa-star fa-sm', onClick: this.deleteImage.bind(this), titleText: 'сделать главной' },
+                ]
             },
             // max-width 800
             {
@@ -62,7 +81,7 @@ export class GalleryComponent implements OnInit {
     }
 
     deleteImage() {
-        console.log('delete image');
+
         this.moveToEnd();
     }
 
@@ -71,6 +90,7 @@ export class GalleryComponent implements OnInit {
         if (this.galleryImages ) {
             let changes = this.differ.diff(this.galleryImages);
             if (changes != null) {
+                this.galleryImages
                 setTimeout(() => this.moveToEnd(), 10);
             }
 
@@ -80,7 +100,7 @@ export class GalleryComponent implements OnInit {
 
 
     moveToEnd() {
-        if (this.gallery_object) {
+        if (this.gallery_object instanceof NgxGalleryComponent ) {
             while (this.gallery_object.canMoveThumbnailsRight()) {
                 this.gallery_object.moveThumbnailsRight();
             }
