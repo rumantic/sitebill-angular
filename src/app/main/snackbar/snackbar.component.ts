255fcc91612765ad71002d396b5c1fc479b72755
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-03-19 02:22:57 +0000

    - snackbar template
    git-svn-id: svn://nanorit.com/jsapi/src@12050 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

--- src/app/main/snackbar/snackbar.component.ts
+++ src/app/main/snackbar/snackbar.component.ts
@@ -1,7 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
+import { MAT_SNACK_BAR_DATA } from "@angular/material";
 
 @Component({
     selector: 'snack-bar-component',
-    template: 'snack message'
+    template: '[innerHTML]="_data.message"'
 })
-export class SnackBarComponent { }
+export class SnackBarComponent {
+    constructor(
+        @Inject(MAT_SNACK_BAR_DATA) private _data: any
+    ) {
+        console.log(_data);
+    }
+
+}
