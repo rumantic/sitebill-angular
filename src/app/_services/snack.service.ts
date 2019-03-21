5e36de73d23149944bb71e73c2e912bb55f3d0a7
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-03-21 03:23:00 +0000

    - snack-сообщения отдельным сервисом
    git-svn-id: svn://nanorit.com/jsapi/src@12105 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/_services/snack.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+    MatSnackBar,
+    MatSnackBarHorizontalPosition,
+    MatSnackBarVerticalPosition,
+} from '@angular/material';
+import { SnackBarComponent } from 'app/main/snackbar/snackbar.component';
+
+
+@Injectable()
+export class SnackService {
+    horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+    verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+    constructor(
+        public snackBar: MatSnackBar,
+    ) {
+    }
+
+    message(message: string) {
+        this.snackBar.openFromComponent(SnackBarComponent, {
+            duration: 3000,
+            horizontalPosition: this.horizontalPosition,
+            verticalPosition: this.verticalPosition,
+            data: { message: message },
+        });
+    }
+}
