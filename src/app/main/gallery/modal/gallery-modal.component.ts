981e7d665765d365c514faf29459cd1da0b64984
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-04-10 06:06:22 +0000

    - gallery-modal.component
    git-svn-id: svn://nanorit.com/jsapi/src@12239 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/main/gallery/modal/gallery-modal.component.ts
@@ -0,0 +1,32 @@
+import { Component, Inject, OnInit} from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+
+import { APP_CONFIG, AppConfig } from 'app/app.config.module';
+import { ModelService } from 'app/_services/model.service';
+
+
+@Component({
+    selector: 'gallery-modal',
+    templateUrl: './gallery-modal.component.html',
+    styleUrls: ['./gallery-modal.component.css']
+})
+export class GalleryModalComponent implements OnInit {
+
+
+
+    constructor(
+        private dialogRef: MatDialogRef<GalleryModalComponent>,
+        private modelSerivce: ModelService,
+        @Inject(APP_CONFIG) private config: AppConfig,
+        @Inject(MAT_DIALOG_DATA) private _data: any
+    ) {
+    }
+
+    ngOnInit() {
+    }
+
+    close() {
+        this.dialogRef.close();
+    }
+
+}
