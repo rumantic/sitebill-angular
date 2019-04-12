c983544af6875f87a5b90bbd1d5ff47c01bd1327
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-04-12 03:35:34 +0000

    git-svn-id: svn://nanorit.com/jsapi/src@12256 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/main/grid/entity/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { GridComponent } from 'app/main/grid/grid.component';
+import { fuseAnimations } from '@fuse/animations';
+
+@Component({
+    selector: 'data-grid',
+    templateUrl: '../../grid.component.html',
+    styleUrls: ['../../grid.component.scss'],
+    animations: fuseAnimations
+})
+export class MenuComponent extends GridComponent {
+    setup_apps() {
+        this.entity.app_name = 'menu';
+        this.entity.primary_key = 'menu_id';
+    }
+}
