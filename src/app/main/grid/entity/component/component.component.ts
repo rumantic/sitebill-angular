b9f0e4c61b5457c7933a4943951d3b063bbb072b
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-04-12 02:37:28 +0000

    git-svn-id: svn://nanorit.com/jsapi/src@12249 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/main/grid/entity/component/component.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { GridComponent } from 'app/main/grid/grid.component';
+import { fuseAnimations } from '@fuse/animations';
+
+@Component({
+    selector: 'data-grid',
+    templateUrl: '../../grid.component.html',
+    styleUrls: ['../../grid.component.scss'],
+    animations: fuseAnimations
+})
+export class ComponentComponent extends GridComponent {
+    setup_apps() {
+        this.entity.app_name = 'component';
+        this.entity.primary_key = 'component_id';
+    }
+}
