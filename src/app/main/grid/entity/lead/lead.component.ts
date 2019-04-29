1253f8f769dc11723a9fcf53d41393644ffd8180
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-04-29 05:08:52 +0000

    - lead.component
    git-svn-id: svn://nanorit.com/jsapi/src@12350 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/main/grid/entity/lead/lead.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { fuseAnimations } from '@fuse/animations';
+
+@Component({
+    selector: 'lead-component',
+    templateUrl: './lead.component.html',
+    styleUrls: ['./lead.component.scss'],
+    animations: fuseAnimations
+})
+export class LeadComponent {
+
+}
