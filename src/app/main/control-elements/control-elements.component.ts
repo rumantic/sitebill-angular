a5994e6128a67dbc1b72aa2ad4ac48b30f124ade
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-05-13 05:37:18 +0000

    - перевел на модель
    git-svn-id: svn://nanorit.com/jsapi/src@12386 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

--- src/app/main/control-elements/control-elements.component.ts
+++ src/app/main/control-elements/control-elements.component.ts
@@ -9,6 +9,7 @@ import {ActivatedRoute} from '@angular/router';
 import {Model} from 'app/model';
 import {currentUser} from 'app/_models/currentuser';
 import { APP_CONFIG, AppConfig } from 'app/app.config.module';
+import { ModelService } from 'app/_services/model.service';
 
 
 @Component({
@@ -54,12 +55,12 @@ export class ControlElementsComponent implements OnInit {
         private _httpClient: HttpClient,
         @Inject(APP_CONFIG) private config: AppConfig,
         private _fuseConfigService: FuseConfigService,
+        protected modelSerivce: ModelService,
         ) {
         this.loadingIndicator = true;
         
         // Set the private defaults
         this._unsubscribeAll = new Subject();
-        this.currentUser = JSON.parse(localStorage.getItem('currentUser')) || [];
         if (isDevMode()) {
             this.api_url = this.config.apiEndpoint;
         } else {
@@ -99,15 +100,16 @@ export class ControlElementsComponent implements OnInit {
         
         let active =  1;
         if (this.item['active']['value'] == 1)  {
-            active = 0;
+            active = null;
         }
-        const ql_items = {active: active};
-        const body = {action: 'model', do: 'graphql_update', model_name: this.model_name, key_value: this.key_value, ql_items: ql_items, session_key: this.currentUser.session_key};
-        
-        this._httpClient.post(`${this.api_url}/apps/api/rest.php`, body)
+        const ql_items = { active: active };
+
+        this.modelSerivce.update_only_ql(this.model_name, this.key_value, ql_items)
             .subscribe((response: any) => {
-                //console.log(response);
-                this.load_item(this.model_name, this.key_value);
+                if (response.state == 'error') {
+                } else {
+                    this.load_item(this.model_name, this.key_value);
+                }
             });
         
     }
@@ -129,24 +131,28 @@ export class ControlElementsComponent implements OnInit {
     }
     
     load_item ( model_name, key_value ) {
-        const body = {action: 'model', do: 'load_data', model_name: this.model_name, key_value: this.key_value, session_key: this.currentUser.session_key};
-        
-        this._httpClient.post(`${this.api_url}/apps/api/rest.php`, body)
+        //const body = { action: 'model', do: 'load_data', model_name: this.model_name, key_value: this.key_value, session_key: this.modelSerivce.get_session_key()};
+
+        this.modelSerivce.loadById(model_name, 'id', key_value)
             .subscribe((response: any) => {
-                //console.log(response);
-                //console.log(this.currentUser.user_id);
-                if ( response.data ) {
-                    
-                    //Проверим что объект принадлежит пользователю
-                    if ( response.data['user_id']['value'] == this.currentUser.user_id ) {
-                        this.item = response.data;
-                        this.current_active_value = this.item['active']['value'];
+                if (response.state == 'error') {
+                } else {
+                    //console.log(this.currentUser.user_id);
+                    if (response.data) {
+
+                        //Проверим что объект принадлежит пользователю
+                        if (response.data['user_id']['value'] == this.modelSerivce.get_user_id()) {
+                            this.item = response.data;
+                            this.current_active_value = this.item['active']['value'];
+                        }
+                        //console.log(this.item);
+                        //console.log(this.item['active']['value']);
                     }
-                    //console.log(this.item);
-                    //console.log(this.item['active']['value']);
+                    this.controlProcessing = false;
                 }
-                this.controlProcessing = false;
             });
+
+
     }
     
     /**
