1cb5ca25c6b62207bad8eb3c8917b21320db2117
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-05-16 04:34:21 +0000

    - bitrix24router - распределяем маршруты к компонентам в зависимости от placement
    git-svn-id: svn://nanorit.com/jsapi/src@12409 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/integrations/bitrix24/bitrix24router.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { FuseConfigService } from "@fuse/services/config.service";
+
+@Component({
+    selector: 'bitrix24router',
+    template: ''
+})
+export class Bitrix24Router {
+    constructor(
+        private router: Router,
+    ) {
+    }
+
+    route(placement: string) {
+        if (placement == 'CRM_CONTACT_DETAIL_TAB' || placement == 'CRM_CONTACT_LIST_MENU') {
+
+            this.router.navigate(['/collections/']);
+        }
+    }
+}
