d59e1cd34f83ca00090418560879012b2d45c409
dmn <dmn@eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d>
2019-05-17 03:20:21 +0000

    - bitrix24.service
    git-svn-id: svn://nanorit.com/jsapi/src@12422 eba385b8-5ad6-4cb3-8d1c-8f45e1fe7a8d

---
+++ src/app/integrations/bitrix24/bitrix24.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Inject, isDevMode } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { APP_CONFIG, AppConfig } from 'app/app.config.module';
+import { Bitrix24Entity } from './bitrix24';
+
+
+@Injectable()
+export class Bitrix24Service {
+    private api_url: string = '';
+    public entity: Bitrix24Entity;
+
+
+    constructor(
+        private http: HttpClient,
+        @Inject(APP_CONFIG) private config: AppConfig,
+    ) {
+        this.entity = new Bitrix24Entity;
+
+        this.set_api_url(localStorage.getItem('api_url'));
+    }
+
+    set_api_url(api_url: string) {
+        this.api_url = api_url;
+    }
+
+    get_api_url() {
+
+        if (isDevMode() && this.api_url == '') {
+            //console.log('dev url');
+            return this.config.apiEndpoint;
+        } else if (this.api_url == null) {
+            return '';
+        } else {
+            //console.log('prod url');
+            return this.api_url;
+        }
+    }
+}
--- src/app/integrations/bitrix24/bitrix24.ts
+++ src/app/integrations/bitrix24/bitrix24.ts
@@ -1,3 +1,17 @@
+export class Bitrix24Entity {
+    private DEAL_ID: number;
+    constructor() {
+        this.set_deal_id(null);
+    }
+
+    set_deal_id(id: number) {
+        this.DEAL_ID = id;
+    }
+    get_deal_id() {
+        return this.DEAL_ID;
+    }
+}
+
 export class Bitrix24PlacementOptions {
     private ID: number;
 
