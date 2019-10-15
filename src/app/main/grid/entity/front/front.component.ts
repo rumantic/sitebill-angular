c7b79a95b9155e1b477f27af38fcfdfc278f76d7
Kondin Dmitriy <dkondin@gmail.com>
2019-10-15 18:12:17 +0700

    - накидал гриды без меню

--- src/app/main/grid/entity/front/front.component.ts
+++ src/app/main/grid/entity/front/front.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { fuseAnimations } from '@fuse/animations';
 import { FilterService } from 'app/_services/filter.service';
 import {ModelService} from '../../../../_services/model.service';
+import {FuseConfigService} from '../../../../../@fuse/services/config.service';
 
 @Component({
     selector: 'front-component',
@@ -13,14 +14,33 @@ export class FrontComponent {
     public allow_load_grid = false;
     constructor(
         private filterService: FilterService,
+        private _fuseConfigService: FuseConfigService,
         protected modelService: ModelService
     ) {
+        this.disable_menu();
         //console.log('lead constructor');
     }
 
     ngOnInit() {
         this.modelService.get_session_key_safe();
     }
+    disable_menu() {
+        this._fuseConfigService.config = {
+            layout: {
+                navbar: {
+                    hidden: true
+                },
+                toolbar: {
+                    hidden: true
+                },
+                footer: {
+                    hidden: true
+                }
+            }
+        };
+    }
+
+
 
     after_validated_key () {
         return this.modelService.is_validated_session_key();
