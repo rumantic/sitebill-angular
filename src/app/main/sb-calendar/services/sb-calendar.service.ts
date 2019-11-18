ee071158a869b4ee9f1348bc7d33f320b35f7529
Abushyk <abushyk@gmail.com>
2019-11-18 14:24:38 +0200

    3

--- src/app/main/sb-calendar/services/sb-calendar.service.ts
+++ src/app/main/sb-calendar/services/sb-calendar.service.ts
@@ -27,11 +27,35 @@ export class SbCalendarService {
     }
     
     edit_rate(object_id, rate_id, params) {
-        console.log('edit');
+        /*console.log('edit');
         console.log(rate_id);
         console.log(object_id);
         console.log(params);
-        //const body = {action: 'reservation', do: 'rate_edit', id: rate_id, session_key: modelService.get_session_key_safe()};
-        //return this.http.post(`${modelService.get_api_url()}/apps/api/rest.php`, body);
+        
+        
+        console.log(params.season_start.toDate());
+        console.log(params.season_start.format('YYYY-MM-DD'));
+        */
+        
+        
+        var r = {
+            month_number: (typeof params.month_number != 'undefined' ? params.month_number : 0),
+            day_number: (typeof params.day_number != 'undefined' ? params.day_number : 0),
+            period_start: (typeof params.period_start != 'undefined' ? params.period_start : ''),
+            period_end: (typeof params.period_end != 'undefined' ? params.period_end : ''),
+            season_start: params.season_start.format('YYYY-MM-DD'),
+            season_end: params.season_end.format('YYYY-MM-DD'),
+            amount: params.amount
+        };
+        //console.log(r);
+        
+        
+        const body = {action: 'reservation', do: 'rate_edit', id: rate_id, data: r, object_id: object_id, session_key: this.modelService.get_session_key_safe()};
+        //console.log(body);
+        //console.log(this.modelService.get_api_url());
+        console.log('send request');
+        //this.http.post(`${this.modelService.get_api_url()}/apps/api/rest1.php`, body)
+        //console.log(this.http.post(`${this.modelService.get_api_url()}/apps/api/rest.php`, body));
+        return this.http.post(`${this.modelService.get_api_url()}/apps/api/rest1.php`, body);
     }
 }
