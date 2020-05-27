d94aa39fae0aab675144d49a9b7013034c00bec6
Kondin Dmitriy <dkondin@gmail.com>
2020-05-27 18:01:45 +0700

    запускается, но не билдится

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,99 +29,39 @@ import {FilterService} from "./_services/filter.service";
 import {SnackService} from "./_services/snack.service";
 import {Bitrix24Router} from "./integrations/bitrix24/bitrix24router";
 import {SnackBarComponent} from "./main/snackbar/snackbar.component";
-import {SearchFormComponent} from "./main/search-form/search-form.component";
 import {CalculatorMiniComponent} from "./main/calculator-mini/calculator-mini.component";
 import {MortgageCalculatorComponent} from "./main/mortgage-calculator/mortgage-calculator.component";
-import {SelectDistrictDialogComponent} from "./main/search-form/dialogs/select-district/select-district.component";
 import {SharedModule} from "./shared.module";
-import {Error404Component} from "./main/pages/errors/404/error-404.component";
 import {LoginComponent} from "./login";
 import {ControlElementsComponent} from "./main/control-elements/control-elements.component";
-import {AnkonsulSearchFormComponent} from "./main/search-form/ankonsul/ankonsul.search-form.component";
-import {CarouselComponent} from "./main/carousel/carousel.component";
-import {SliderComponent} from "./main/slider/slider.component";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatSelectModule} from "@angular/material/select";
+import {MatTableModule} from "@angular/material/table";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: 'frontend', pathMatch: 'full'},
-    {path: 'login', component: LoginComponent},
-    {path: 'logout', component: LoginComponent},
-    {
-        path: 'control/:model_name/:id/:control_name',
-        component: ControlElementsComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'slider',
-        component: SliderComponent
-    },
-    {
-        path: 'search',
-        component: SearchFormComponent
-    },
-    {
-        path: 'searchankonsul',
-        component: AnkonsulSearchFormComponent
-    },
-    {
-        path: 'carousel',
-        component: CarouselComponent
-    },
-    {
-        path: 'calculator',
-        component: MortgageCalculatorComponent
-    },
-    {
-        path: 'calculator-mini',
-        component: CalculatorMiniComponent
-    },
+    {path: '', redirectTo: 'sample', pathMatch: 'full'},
     {
         path: 'sample',
         loadChildren: 'app/main/sample/sample.module#SampleModule'
-    },
-    {
-        path: 'grid',
-        loadChildren: 'app/main/grid/grid.module#GridModule',
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'public',
-        loadChildren: 'app/main/grid/grid.module#GridModule',
-        canActivate: [PublicGuard],
-        runGuardsAndResolvers: 'always'
-    },
-    {
-        path: 'facebook',
-        loadChildren: 'app/main/facebook/facebook.module#FacebookModule'
-    },
-    {
-        path: 'vk',
-        loadChildren: 'app/main/vk/vk.module#VkModule'
-    },
-    {
-        path: 'profile',
-        loadChildren: 'app/main/profile/profile.module#ProfileModule',
-        canActivate: [PublicGuard],
-    },
-    {
-        path: 'frontend',
-        loadChildren: 'app/main/frontend/frontend.module#FrontendModule'
-    },
-    {
-        path: 'dashboard',
-        loadChildren: 'app/main/dashboard/dashboard.module#DashboardModule',
-        canActivate: [PublicGuard],
-    },
-    {
-        path: 'cart/:step/:item_id',
-        loadChildren: 'app/main/cart/cart.module#CartModule'
-    },
-    {
-        path: 'cart/:step',
-        loadChildren: 'app/main/cart/cart.module#CartModule'
-    },
-    {
-        path: 'calendar',
-        loadChildren: 'app/main/sb-calendar/sb-calendar.module#SbCalendarModuleWithRoutes',
     },
     /*
     {
@@ -135,10 +75,6 @@ const appRoutes: Routes = [
 
     },
     */
-    {
-        path: '**',
-        component: Error404Component
-    }
 ];
 
 @NgModule({
@@ -161,6 +97,28 @@ const appRoutes: Routes = [
         // Material
         MatButtonModule,
         MatIconModule,
+        MatCheckboxModule,
+        MatGridListModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatMenuModule,
+        MatSelectModule,
+        MatTableModule,
+        MatTabsModule,
+        MatDialogModule,
+        MatDatepickerModule,
+        MatProgressSpinnerModule,
+        MatTooltipModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatRadioModule,
+        MatCardModule,
+        MatInputModule,
+        MatSliderModule,
+        MatAutocompleteModule,
+        MatSnackBarModule,
+        MatButtonToggleModule,
+        MatExpansionModule,
 
         // Fuse modules
         FuseModule.forRoot(fuseConfig),
