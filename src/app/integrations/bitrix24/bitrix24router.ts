b7a84253102efa1c3a6a6ef44b0e7345024cdc43
Kondin Dmitriy <dkondin@gmail.com>
2020-07-16 10:08:19 +0700

    с помощью этих условий можно будет роутить по условию в битрикс24 только один раз

--- src/app/integrations/bitrix24/bitrix24router.ts
+++ src/app/integrations/bitrix24/bitrix24router.ts
@@ -20,11 +20,16 @@ export class Bitrix24Router {
         if (this.modelSerivce.get_user_id() == null) {
             return false;
         }
+        if ( !this.modelSerivce.is_config_loaded() ) {
+            //return false;
+        }
+
         if ( !this.first_run ) {
-            return false;
+            //return false;
         }
 
         if (placement == 'CRM_CONTACT_DETAIL_TAB' || placement == 'CRM_CONTACT_LIST_MENU' || placement == 'CRM_DEAL_LIST_MENU' || placement == 'CRM_DEAL_DETAIL_TAB' || placement == 'CRM_LEAD_LIST_MENU' || placement == 'CRM_LEAD_DETAIL_TAB') {
+            //console.log('bitrix route ' + this.first_run);
             this.first_run = false;
             this.router.navigate(['/grid/collections/']);
         }
