58f193b4673a36530aea95073efabec82bb8f571
Kondin Dmitriy <dkondin@gmail.com>
2020-07-29 11:07:32 +0700

    убрал тесты

--- src/app/main/grid/grid.component.ts
+++ src/app/main/grid/grid.component.ts
@@ -895,24 +895,6 @@ export class GridComponent implements OnInit, OnDestroy
         // console.log(this.bitrix24Service.get_placement());
         let title = 'bitrix deal ' + this.bitrix24Service.get_entity_id();
 
-        this.bitrix24Service.user_option_get()
-            .subscribe((response: any) => {
-                    //console.log(response);
-                }
-            );
-
-        console.log(this.bitrix24Service.get_placement_options().get_user_option().get_value('mister'));
-        console.log(this.bitrix24Service.get_placement_options().get_user_option().get_value('mister_null'));
-        console.log(this.bitrix24Service.get_placement_options().get_user_option().get_value('session_key'));
-        //this.bitrix24Service.pla
-
-        let storage = JSON.parse(localStorage.getItem('currentUser')) || [];
-        this.bitrix24Service.user_option_set(storage)
-            .subscribe((response: any) => {
-                    console.log(response);
-                }
-            );
-
         this.modelService.toggle_collections(this.bitrix24Service.get_domain(), this.bitrix24Service.get_entity_id(), title, data_id)
             .subscribe((response: any) => {
                 console.log(response);
