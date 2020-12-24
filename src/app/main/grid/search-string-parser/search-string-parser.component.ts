3cc022c6fa880ff18867172b9a625360168dc4ac
Kondin Dmitriy <dkondin@gmail.com>
2020-12-24 12:23:23 +0700

    добавил подписку на изменение строчки поиска

--- src/app/main/grid/search-string-parser/search-string-parser.component.ts
+++ src/app/main/grid/search-string-parser/search-string-parser.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 
 import { fuseAnimations } from '@fuse/animations';
 
 
-import { Subject } from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import { ModelService } from 'app/_services/model.service';
+import {FilterService} from "../../../_services/filter.service";
+import {FormControl} from "@angular/forms";
+import {SitebillEntity} from "../../../_models";
 
 
 @Component({
@@ -19,12 +22,24 @@ export class SearchStringParserComponent implements OnInit, OnDestroy
     // Private
     private _unsubscribeAll: Subject<any>;
 
+    @Input('search_string')
+    search_string: string;
+
+    @Input('entity')
+    entity: SitebillEntity;
+
+
+    parsed_string: string;
+    public result_message: string;
+
+
     /**
      * Constructor
      *
      */
     constructor(
         private modelService: ModelService,
+        public filterService: FilterService,
     )
     {
         // Set the private defaults
@@ -40,6 +55,17 @@ export class SearchStringParserComponent implements OnInit, OnDestroy
      */
     ngOnInit(): void
     {
+        this.filterService.share.subscribe((entity: SitebillEntity) => {
+            if (entity.get_app_name() == this.entity.get_app_name()) {
+                this.parse();
+            }
+        });
+    }
+
+    parse() {
+        let parse_array = this.search_string.split(' ');
+        this.result_message = '1 комн, 50 м';
+        console.log(parse_array);
     }
 
 
