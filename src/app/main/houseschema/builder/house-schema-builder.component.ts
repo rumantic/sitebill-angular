e502d0c1810c1d32f36555293c3fced6611644f3
Kondin Dmitriy <dkondin@gmail.com>
2021-01-21 15:05:54 +0700

    добавил fabric

--- src/app/main/houseschema/builder/house-schema-builder.component.ts
+++ src/app/main/houseschema/builder/house-schema-builder.component.ts
@@ -8,6 +8,7 @@ import {SitebillEntity} from "../../../_models";
 import {FormComponent} from "../../grid/form/form.component";
 import {NgxGalleryImage} from "ngx-gallery-9";
 import {SafeResourceUrl} from "@angular/platform-browser";
+import { fabric } from "fabric";
 
 @Component({
     selector   : 'house-schema-builder',
@@ -16,6 +17,14 @@ import {SafeResourceUrl} from "@angular/platform-browser";
 })
 export class HouseSchemaBuilderComponent
 {
+    public canvas: any;
+    public textString: string;
+    private size: any = {
+        width: 1200,
+        height: 1000
+    };
+    public OutputContent: string;
+
     @Input("_data")
     _data: {
         entity: SitebillEntity,
@@ -45,6 +54,93 @@ export class HouseSchemaBuilderComponent
             // console.log(this._data);
         } else {
             this.schema_url = "https://qplan.sitebill.site/img/data/20210121/jpg_6008feb8a8b6e_1611202232_1.svg?1611206294340";
+        }
+
+        this.canvas = new fabric.Canvas('canvas', {
+            hoverCursor: 'pointer',
+            selection: true,
+            selectionBorderColor: 'blue'
+        });
+        this.textString = null;
+        this.canvas.setWidth(this.size.width);
+        this.canvas.setHeight(this.size.height);
+        this.OutputContent = null;
+    }
+
+    addText() {
+        let textString = this.textString;
+        let text = new fabric.IText(textString, {
+            left: 10,
+            top: 10,
+            fontFamily: 'helvetica',
+            angle: 0,
+            fill: '#000000',
+            scaleX: 0.5,
+            scaleY: 0.5,
+            fontWeight: '',
+            hasRotatingPoint: true
+        });
+        this.extend(text, this.randomId());
+        this.canvas.add(text);
+        this.selectItemAfterAdded(text);
+        this.textString = '';
+    }
+
+    extend(obj, id) {
+        obj.toObject = (function (toObject) {
+            return function () {
+                return fabric.util.object.extend(toObject.call(this), {
+                    id: id
+                });
+            };
+        })(obj.toObject);
+    }
+    //======= this is used to generate random id of every object ===========
+    randomId() {
+        return Math.floor(Math.random() * 999999) + 1;
+    }
+    //== this function is used to active the object after creation ==========
+    selectItemAfterAdded(obj) {
+        this.canvas.discardActiveObject().renderAll();
+        this.canvas.setActiveObject(obj);
+    }
+
+    addFigure(figure) {
+        let add: any;
+        switch (figure) {
+            case 'rectangle':
+                add = new fabric.Rect({
+                    width: 200, height: 100, left: 10, top: 10, angle: 0,
+                    fill: '#3f51b5'
+                });
+                break;
+            case 'square':
+                add = new fabric.Rect({
+                    width: 100, height: 100, left: 10, top: 10, angle: 0,
+                    fill: '#4caf50'
+                });
+                break;
+            case 'triangle':
+                add = new fabric.Triangle({
+                    width: 100, height: 100, left: 10, top: 10, fill: '#2196f3'
+                });
+                break;
+            case 'circle':
+                add = new fabric.Circle({
+                    radius: 50, left: 10, top: 10, fill: '#ff5722'
+                });
+                break;
+        }
+        this.extend(add, this.randomId());
+        this.canvas.add(add);
+        this.selectItemAfterAdded(add);
+    }
+
+    ExportToContent(input) {
+        if(input == 'json'){
+            this.OutputContent = JSON.stringify(this.canvas);
+        }else if(input == 'svg'){
+            this.OutputContent = this.canvas.toSVG();
         }
     }
 }
