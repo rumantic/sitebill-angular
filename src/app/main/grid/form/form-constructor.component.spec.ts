609bfaf5fc82f94220ee7bebdaadbe22f4554520
LevKhaak <leu.khaak@gmail.com>
2022-06-24 23:10:37 +0300

    test new error 24.06

--- src/app/main/grid/form/form-constructor.component.spec.ts
+++ src/app/main/grid/form/form-constructor.component.spec.ts
@@ -9,47 +9,58 @@ import { Bitrix24Service } from 'app/integrations/bitrix24/bitrix24.service';
 import {MatDialog} from '@angular/material/dialog';
 import { StorageService } from 'app/_services/storage.service';
 import {SitebillEntity} from '../../../_models';
+import { TestDataFake } from './test-data';
+import {Observable, Observer} from 'rxjs';
 
 describe('FormConstructorComponent', () => {
 
     let component: FormConstructorComponent;
     let fixture: ComponentFixture<FormConstructorComponent>;
+    const fakeData = new Observable((sub) => {
+        sub.next(TestDataFake.data);
+    });
+    let val = 'OOO';
+    fakeData.subscribe((vl) => {this.val = vl; });
 
-    // const fakeMatDialogRef = jasmine.createSpyObj('fakeMatDialogRef', ['close']);
-    const fakeModelService = {...jasmine.createSpyObj('fakeModelService', ['get_access', 'get_user_id', 'get_api_url', 'get_app_name']), entity: new SitebillEntity()};
+    const fakeModelService = {...jasmine.createSpyObj('fakeModelService',
+            ['get_access', 'get_user_id', 'get_api_url', 'get_app_name', 'loadById']), entity: new SitebillEntity()};
+    const xxx = fakeModelService.loadById.and.returnValue(val); // вернуть обзервйбл
     const fakeFormBuilder = jasmine.createSpyObj('fakeFormBuilder', ['group']);
     const fakeSnackService = jasmine.createSpyObj('fakeSnackService', ['message']);
     const fakeMatDialog = jasmine.createSpyObj('fakeMatDialog', ['open']);
     const fakeFilterService = {};
-    const fakeBitrix24Service = {};
-    // const fakeAppConfig = {};
-    // const fakeMatDialogData = jasmine.createSpyObj('fakeMatDialogData', ['set_readonly', 'is_delete_disabled', 'get_key_value', 'get_table_name']);
+    const fakeBitrix24Service = jasmine.createSpyObj('fakeBitrix24Service',
+        ['init_input_parameters', 'is_bitrix24_inited']);
     const fakeStorageService = jasmine.createSpyObj('fakeStorageService', ['getItem']);
 
-    beforeEach(async () => { //
-        TestBed.overrideComponent(FormConstructorComponent, {set: {providers: []}});
+    beforeEach(async () => { // _unsubscribeAll
         await TestBed.configureTestingModule({
             declarations: [ FormConstructorComponent ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
             providers: [
-                // { provide: MatDialogRef, useValue: fakeMatDialogRef },
                 { provide: ModelService, useValue: fakeModelService },
                 { provide: FormBuilder, useValue: fakeFormBuilder },
                 { provide: SnackService, useValue: fakeSnackService },
                 { provide: MatDialog, useValue: fakeMatDialog },
                 { provide: FilterService, useValue: fakeFilterService },
                 { provide: Bitrix24Service, useValue: fakeBitrix24Service },
-                // { provide: APP_CONFIG, useValue: fakeAppConfig },
-                // { provide: MAT_DIALOG_DATA, useValue: fakeMatDialogData },
                 { provide: StorageService, useValue: fakeStorageService },
-             ]
+            ]
         })
             .compileComponents();
     });
     beforeEach(() => {
         fixture = TestBed.createComponent(FormConstructorComponent);
         component = fixture.componentInstance;
-        console.log(component);
+
+        let entity = new SitebillEntity();
+        entity.set_table_name('data');
+        entity.set_app_name('data');
+        entity.set_primary_key('id');
+        entity.set_title('Data test');
+        component._data = entity;
+        // console.log(component);
+        console.log('xxx',  fakeData);
         fixture.detectChanges();
     });
 
