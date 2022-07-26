5ee6176ac5daa8cd9c17e3fe828cd5e94705cd27
LevKhaak <leu.khaak@gmail.com>
2022-07-26 09:40:09 +0300

    ui-service used

--- src/app/_guards/public.guard.ts
+++ src/app/_guards/public.guard.ts
@@ -1,11 +1,13 @@
-import { AuthGuard } from ".";
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
-import { FuseNavigationService } from "@fuse/components/navigation/navigation.service";
-import { FuseConfigService } from "@fuse/services/config.service";
-import { ModelService } from "app/_services/model.service";
-import { SnackService } from "app/_services/snack.service";
-import {StorageService} from "../_services/storage.service";
+import { AuthGuard } from '.';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { FuseNavigationService } from '@fuse/components/navigation/navigation.service';
+import { FuseConfigService } from '@fuse/services/config.service';
+import { ModelService } from 'app/_services/model.service';
+import { SnackService } from 'app/_services/snack.service';
+import {StorageService} from '../_services/storage.service';
+import {GetSessionKeyService} from '../_services/get-session-key.service';
+import {UiService} from '../_services/ui.service';
 
 @Injectable()
 export class PublicGuard extends AuthGuard {
@@ -13,6 +15,8 @@ export class PublicGuard extends AuthGuard {
         router: Router,
         protected modelService: ModelService,
         protected storageService: StorageService,
+        protected getSessionKeyService: GetSessionKeyService,
+        protected uiService: UiService,
         _fuseNavigationService: FuseNavigationService,
         _fuseConfigService: FuseConfigService,
         protected _snackService: SnackService,
@@ -20,22 +24,24 @@ export class PublicGuard extends AuthGuard {
         super(
             router,
             modelService,
+            getSessionKeyService,
+            uiService,
             storageService,
             _fuseNavigationService,
             _fuseConfigService,
             _snackService
         );
-        //this._fuseNavigationService.removeNavigationItem('page');
+        // this._fuseNavigationService.removeNavigationItem('page');
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        //console.log('public canActivate');
+        // console.log('public canActivate');
         return this.check_session(route, state, '/');
     }
     check_permissions(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        //@todo: Нужно проверять текущую сессию на пригодность
-        //console.log('Activate result = ');
-        //console.log('check permission public');
+        // @todo: Нужно проверять текущую сессию на пригодность
+        // console.log('Activate result = ');
+        // console.log('check permission public');
         this.set_public_menu();
 
         let navigation_origin = this._fuseNavigationService.getNavigation('main');
@@ -44,16 +50,16 @@ export class PublicGuard extends AuthGuard {
         this.cleanUpNavigation(navigtaion_clone, storage['structure']);
 
         if (storage['structure'] == null) {
-            this.modelService.logout();
+            this.getSessionKeyService.logout();
             return false;
         }
 
         if (storage['structure']['group_name'] == null) {
-            this.modelService.logout();
+            this.getSessionKeyService.logout();
             return false;
         }
 
-        //console.log('complete cleanUp');
+        // console.log('complete cleanUp');
 
         // logged in so return true
         return true;
